bf464a97e23181db7613c5bd9e9aa5041e68975f
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-07-12 13:40:48 -0700

    back link button added

--- app/code/repos/[name]/page.tsx
+++ app/code/repos/[name]/page.tsx
@@ -1,8 +1,12 @@
 import Repo from "@/app/components/Repo";
+import Link from "next/link";
 
 const RepoPage = ({ params }: { params: { name: string } }) => {
   return (
     <div className="card">
+      <Link href="/code/repos" className="btn btn-back">
+        Back to Repositories
+      </Link>
       <Repo name={params.name} />
     </div>
   );
